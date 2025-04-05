93cbf46e476a773a13a71b71a5c394f318ec6bac
Luis Muñoz <l.munoz.m@outlook.com>
2025-04-05 18:36:59 -0300

    Styling using PICO css and local stylesheet.

--- index.php
+++ index.php
@@ -13,7 +13,7 @@ $data = json_decode($response, true);
 ?>
 
 <!DOCTYPE html>
-<html lang="en">
+<html lang="en" data-theme="dark">
 
 <head>
   <meta charset="UTF-8" />
@@ -21,33 +21,28 @@ $data = json_decode($response, true);
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
   <title>Next Marvel Movie</title>
   <link rel="stylesheet" href="style.css" />
+  <link
+    rel="stylesheet"
+    href="https://cdn.jsdelivr.net/npm/@picocss/pico@2/css/pico.classless.min.css" />
+
 </head>
 
 <body>
-  <h1>Next Marvel Movie</h1>
-  <?php if (!empty($data)): ?>
-    <p>The next Marvel movie is: <?php echo $data['title']; ?></p>
-    <p>Release date: <?php echo $data['release_date']; ?></p>
-    <p>Days until release: <?php echo $data['days_until']; ?></p>
-    <p>Overview: <?php echo $data['overview']; ?></p>
-    <img src="<?php echo $data['poster_url']; ?>" alt="Poster" />
-    <p>Folowing movie: <?php echo $data['following_production']['title']; ?> in <?php echo $data['following_production']['days_until']; ?> days.</p>
-  <?php else: ?>
-    <p>No data found</p>
-  <?php endif; ?>
+  <main>
+    <section class="center">
+      <?php if (!empty($data)): ?>
+        <h2><mark><?php echo $data['title']; ?></mark> releases in <?php echo $data['days_until']; ?> days!</h2>
+        <p class="movie-release-date">
+          Release date: <?php echo $data['release_date']; ?>
+        </p>
+        <p class="movie-overview"><?php echo $data['overview']; ?></p>
+        <img src="<?php echo $data['poster_url']; ?>" alt="<?php echo $data['title']; ?>Poster" class="poster" />
+        <p class="movie-following-movie">Folowing movie: <?php echo $data['following_production']['title']; ?> in <?php echo $data['following_production']['days_until']; ?> days.</p>
+      <?php else: ?>
+        <p>No data found</p>
+      <?php endif; ?>
+    </section>
+  </main>
 </body>
 
 </html>
-
-<?php
-##check if the data is not empty
-#if (!empty($data)) {
-#  echo '
-#<pre>';
-#  print_r($data);
-#  echo '</pre>';
-#} else {
-#  echo 'No data found';
-#}
-#
-?>
